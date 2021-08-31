00d32d9c671c41a0a567ba2440f80d2911c9dec6
Jose Aparcana <josered30@hotmail.com>
2021-08-31 02:13:44 -0500

    .

--- src/aternos/aternos.js
+++ src/aternos/aternos.js
@@ -147,6 +147,8 @@ async function connect(id, req) {
 
     await page.goto(startPage);
 
+    await page.screenshot({ path: "screenshot.png" });
+
     await page.type("#user", process.env.ATERNOS_USER);
     await page.type("#password", process.env.ATERNOS_PASSWORD);
     await page.click("#login");
