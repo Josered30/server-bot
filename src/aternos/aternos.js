d0227a4c4c425e40bc2d6448d921ead972fc72fc
Jose Aparcana <josered30@hotmail.com>
2021-08-30 19:22:02 -0500

    .

--- src/aternos/aternos.js
+++ src/aternos/aternos.js
@@ -137,6 +137,7 @@ async function connect(id, req) {
     browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
+    
 
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080 });
