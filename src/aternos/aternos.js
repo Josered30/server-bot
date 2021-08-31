88a87b7027b571e2c6fae3bfb86c57d4da2f00bf
Jose Aparcana <josered30@hotmail.com>
2021-08-31 02:57:53 -0500

    .

--- src/aternos/aternos.js
+++ src/aternos/aternos.js
@@ -143,7 +143,6 @@ async function connect(id, req) {
     });
 
     const page = await browser.newPage();
-    
     await page.setUserAgent(randomUserAgent.getRandom());
     await page.setViewport({ width: 1920, height: 1080 });
 
